ac9bc58c4f6d16b0d91edef19bf2eacbab92a86f
OvOPlant <149712688+OvOPlant@users.noreply.github.com>
2024-02-15 22:55:35 -0500

    Update tas.js

--- modloader/mods/dim/tas.js
+++ modloader/mods/dim/tas.js
@@ -1,33 +1,264 @@
-let old = globalThis.sdk_runtime;
-c2_callFunction("execCode", ["globalThis.sdk_runtime = this.runtime"]);
-let runtime = globalThis.sdk_runtime;
-globalThis.sdk_runtime = old;
+(function() {
+    let old = globalThis.sdk_runtime;
+    c2_callFunction("execCode", ["globalThis.sdk_runtime = this.runtime"]);
+    let runtime = globalThis.sdk_runtime;
+    globalThis.sdk_runtime = old;
+    var aaaa = runtime.groups_by_name["gameplay - timescale"].subevents[1].actions[1].parameters[0].object
+    let notify = (title, text, image = "https://ovoplant.github.io/ovo/versions/reverse/velocity.png") => {
+        cr.plugins_.sirg_notifications.prototype.acts.AddSimpleNotification.call(
+            runtime.types_by_index.find(
+                (type) => type.plugin instanceof cr.plugins_.sirg_notifications
+            ).instances[0],
+            title,
+            text,
+            image
+        );
+    };
+    let timeScale = {
+        init() {
+            runtime.tickMe(TimeScale);
+            var e = []
+            globalThis.ovoTimescale = {loadInputs: function (t) { e = "number" == typeof t[0][0] ? t : t.map(function (t, n) { return [1 * n / 60, t] }) }, get loadedInputs() { return e }, playInputs: function () { timescale = 1; var t = 0, n = { tick: function () { var o = runtime.dt; for (t += o, console.log(t, o, e.length); 0 < e.length && t > e[0][0];)t -= 1 / 60, e.shift()[1].forEach(function (t) { "Restart" === t ? c2_callFunction("Menu > Replay") : "Jump" === t ? c2_callFunction("Controls > Buffer", ["Jump"]) : c2_callFunction("Controls > " + t) }); 0 === e.length && runtime.untickMe(n) } }; runtime.tickMe(n) }};
+            notify("How to use", "In any way you can, change the variable 'timescale' to a number to change the game's timescale.",
+            "https://ovoplant.github.io/ovo/versions/reverse/electric.png")
+            setTimeout(function() {
+                notify("TAS Keybinds", "This includes both versions of CTD's TAS Keybinds mod. To enable version 1, set 'tasKeybinds' to 1. To enable version 2, set 'tasKeybinds' to 2.")
+            },4000)
+            setTimeout(function() {
+                notify("Wall Warping", "Set 'tasKeybinds' to 3 to activate wall warping. Too much to explain here, ask in the Discord if you need help.")
+            },8000)
+            setTimeout(function() {
+                notify("Take notes", "Due to lack of a replay system, you'll have to quickly write your times somewhere",
+                "https://ovoplant.github.io/ovo/versions/reverse/electric.png")
+            },12000)
+        },
+    }
+    timescale = 1
+    var TSRestore, bindsHelp1, bindsHelp2, bindsHelp3, player;
+    let TimeScale = {
+        tick(){
+            player = runtime.types_by_index.filter((x) =>!!x.animations &&x.animations[0].frames[0].texture_file.includes("collider"))[0].instances.filter((x) => x.instance_vars[18] === "")[0];
+            if (timescale === null || timescale === "" || timescale < 0 || isNaN(timescale)){
+                timescale = TSRestore
+                notify("Oops", "You must set the timescale to a positive number.", "https://ovoplant.github.io/ovo/versions/reverse/electric.png")
+            }   
+            if (runtime.running_layout.sheetname === "Main" || runtime.running_layout.sheetname === "Reward"){
+                runtime.groups_by_name["gameplay - timescale"].subevents[1].actions[1].parameters[0].object = aaaa
+                runtime.timescale = 1
+            }
+            TSRestore = timescale
+            if (runtime.running_layout.sheetname === "Levels"){
+            if (!runtime.running_layout.layers.find(x=>x.name === "Pause").visible){
+                if (runtime.running_layout.layers.find(x=>x.name === "Win").visible){
+                    runtime.groups_by_name["gameplay - timescale"].subevents[1].actions[1].parameters[0].object = aaaa
+                    runtime.timescale = 1
+                }else{
+                runtime.groups_by_name["gameplay - timescale"].subevents[1].actions[1].parameters[0].object = null
+                runtime.timescale = timescale
+                var i, len
+                for (i = 0, len = aaaa.instances.length; i < len; i++){
+                    
+                        aaaa.instances[i].my_timescale = runtime.timescale
+                }
+            };
+            }else if (runtime.running_layout.layers.find(x=>x.name === "Pause").visible){
+                runtime.groups_by_name["gameplay - timescale"].subevents[1].actions[1].parameters[0].object = aaaa
+            }
+        }
+        if (tasKeybinds !== 3){
+            b.style.opacity = 0
+            clearInterval(bindsHelp3)
+            bindsHelp3 = setInterval(function() {
+                if (tasKeybinds === 3){
+                    notify("yoooooo", "Q = 1<br/>A = 0.02<br/>Z = 0.001<br/>X = 0.0001<br/>C = 0.00001")
+                    clearInterval(bindsHelp3)
+                }
+            })
+        }else if (tasKeybinds === 3){
+            b.style.opacity = 0.7
+            b.innerHTML = player.x.toString() + "\n" + player.y.toString();
+        }
+    }
+}
+tasKeybinds = 0
+setInterval(function() {
+    if (tasKeybinds !== 1){
+        clearInterval(bindsHelp1)
+        bindsHelp1 = setInterval(function() {
+            if (tasKeybinds === 1){
+                notify("yoooooo", "set timescale with qweasdzxcv, t is tas jump, g is jump spam")
+                clearInterval(bindsHelp1)
+            }
+        })
+    }else if (tasKeybinds !== 2){
+        clearInterval(bindsHelp2)
+        bindsHelp2 = setInterval(function() {
+            if (tasKeybinds === 2){
+                notify("yoooooo", "Look at <a href='https://arjun99ab.github.io/ovo/src/mods/modloader/taskeybinds.png' target='_blank'>this image<a> for help.")
+                clearInterval(bindsHelp2)
+            }
+        })
+    }
+})
+    
 
-let notify = (title, text, image = "./speedrunner.png") => {
-    cr.plugins_.sirg_notifications.prototype.acts.AddSimpleNotification.call(
-      runtime.types_by_index.find(
-        (type) => type.plugin instanceof cr.plugins_.sirg_notifications
-      ).instances[0],
-      title,
-      text,
-      image
-    );
-};
-notify("TAS Tools Loaded", "Join the <a href='https://discord.gg/ovo-speedrunning-community-645935373368688650' target='_blank'>Discord<a> for more info", "https://thenounproject.com/api/private/icons/2473944/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0");
+// CoolTasDude's TAS Keybinds mod. To enable, set variable 'tasKeybinds' to 1.
+document.addEventListener("keydown", (event) => {
+    if (runtime.running_layout.sheetname === "Levels"){
+        if (tasKeybinds === 1){
+            if (event.code === "KeyA") {
+                timescale = 0.2
+                    notify("timescale set to 0.2");
+                }
+                if (event.code === "KeyS") {
+                timescale = 0.05
+                    notify("timescale set to 0.05");
+                }
+                if (event.code === "KeyD") {
+                timescale = 0.02
+                    notify("timescale set to 0.02");
+                }
+                if (event.code === "KeyQ") {
+                timescale = 1
+                    notify("timescale set to 1");
+                }
+                if (event.code === "KeyW") {
+                timescale = 0.1
+                    notify("timescale set to 0.1");
+                }
+                if (event.code === "KeyE") {
+                timescale = 0.01
+                    notify("timescale set to 0.01");
+                }
+                if (event.code === "KeyZ") {
+                timescale = 2
+                    notify("timescale set to 2");
+                }
+                if (event.code === "KeyX") {
+                timescale = 0.5
+                    notify("timescale set to 0.5");
+                }
+                if (event.code === "KeyC") {
+                timescale = 0.005
+                    notify("timescale set to 0.005");
+                }
+                if (event.code === "KeyV") {
+                timescale = 0.003
+                    notify("timescale set to 0.003");
+                }
+                if (event.code === "KeyT") {
+                    ovoTimescale.loadInputs([["jump"],])
+                    ovoTimescale.playInputs()
+                    notify("tas jump inputed");
+                }
+                if (event.code === "KeyG") {
+                    ovoTimescale.loadInputs([["Jump"],["Jump"],["Jump"],["Jump"],["Jump"],])
+                    ovoTimescale.playInputs()
+                    notify("auto mj inputed");
+            }
+        }else if (tasKeybinds === 2){
+            if (event.code === "KeyQ") {
+                timescale = 1
+                notify("timescale set to 1");
+            }
+            if (event.code === "KeyW") {
+                timescale = 0.5
+                notify("timescale set to 0.5");
+            }
+            if (event.code === "KeyE") {
+                timescale = 0.02
+                notify("timescale set to 0.02");
+            }
+            if (event.code === "KeyA") {
+                timescale = 0.2
+                notify("timescale set to 0.2");
+            }
+            if (event.code === "KeyD") {
+                if (event.shiftKey) {
+                timescale = 0.07
+                notify("timescale set to 0.07");
+                } else {        
+                timescale = 0.05
+                notify("timescale set to 0.05");
+                }
+            }
+            if (event.code === "KeyS") {
+                timescale = 0.1
+                notify("timescale set to 0.1");
+            }
+            if (event.code === "KeyX") {
+                timescale *= 2
+                notify("timescale set to " + timescale);
+            }
+            if (event.code === "KeyZ") {
+                timescale *= 5
+                notify("timescale set to " + timescale);
+            }
+            if (event.code === "KeyC") {
+                timescale /= 2
+                notify("timescale set to " + timescale);
+            }
+            if (event.code === "KeyV") {
+                timescale /= 5
+                notify("timescale set to " + timescale);
+            }
+            if(event.code === "KeyF") {
+                if (event.shiftKey) {
+                    ovoTimescale.loadInputs([["Down"],])
+                    ovoTimescale.playInputs()
+                    notify("down input inputed");
+                } else {
+                    ovoTimescale.loadInputs([["Jump"],["Jump"],["Jump"],["Jump"],["Jump"],])
+                    ovoTimescale.playInputs()
+                    notify("auto mj inputed");
+                }
+            }
+        }else if (tasKeybinds === 3){
+            if (event.code === "KeyQ") {
+                timescale = 1
+                  notify("timescale set to 1");
+              }
+              if (event.code === "KeyA") {
+                timescale = 0.02
+                  notify("timescale set to 0.02");
+              }
+           if (event.code === "KeyZ") {
+                timescale = 0.001
+                  notify("timescale set to 0.001");
+              }
+              if (event.code === "KeyX") {
+                timescale = 0.0001
+                  notify("timescale set to 0.0001");
+              }
+              if (event.code === "KeyC") {
+                timescale = 0.00001
+                  notify("timescale set to 0.00001");
+              }          
+        }
+    }
+});
+  var b = document.createElement("div"),
+    c = {
+      backgroundColor: "rgba(150,10,1)",
+      opacity: 0,
+      width: "300px",
+      height: "60px",
+      position: "absolute",
+      top: "100px",
+      left: "100px",
+      userSelect: "none",
+      fontSize: "x-large",
+    };
+  Object.keys(c).forEach(function (a) {
+    b.style[a] = c[a];
+  });
+  b.id = "TAS";
+  var newContent = document.createTextNode("coords");
 
-//minified version of the code below, so people can't reverse engineer it & cheat runs
-var $jscomp=$jscomp||{};$jscomp.scope={};$jscomp.arrayIteratorImpl=function(a){var b=0;return function(){return b<a.length?{done:!1,value:a[b++]}:{done:!0}}};$jscomp.arrayIterator=function(a){return{next:$jscomp.arrayIteratorImpl(a)}};$jscomp.makeIterator=function(a){var b="undefined"!=typeof SymbWol&&Symbol.iterator&&a[Symbol.iterator];return b?b.call(a):$jscomp.arrayIterator(a)};$jscomp.arrayFromIterator=function(a){for(var b,c=[];!(b=a.next()).done;)c.push(b.value);return c};
-$jscomp.arrayFromIterable=function(a){return a instanceof Array?a:$jscomp.arrayFromIterator($jscomp.makeIterator(a))};$jscomp.ASSUME_ES5=!1;$jscomp.ASSUME_NO_NATIVE_MAP=!1;$jscomp.ASSUME_NO_NATIVE_SET=!1;$jscomp.defineProperty=$jscomp.ASSUME_ES5||"function"==typeof Object.defineProperties?Object.defineProperty:function(a,b,c){a!=Array.prototype&&a!=Object.prototype&&(a[b]=c.value)};
-$jscomp.getGlobal=function(a){return"undefined"!=typeof window&&window===a?a:"undefined"!=typeof global&&null!=global?global:a};$jscomp.global=$jscomp.getGlobal(this);$jscomp.SYMBOL_PREFIX="jscomp_symbol_";$jscomp.initSymbol=function(){$jscomp.initSymbol=function(){};$jscomp.global.Symbol||($jscomp.global.Symbol=$jscomp.Symbol)};$jscomp.Symbol=function(){var a=0;return function(b){return $jscomp.SYMBOL_PREFIX+(b||"")+a++}}();
-$jscomp.initSymbolIterator=function(){$jscomp.initSymbol();var a=$jscomp.global.Symbol.iterator;a||(a=$jscomp.global.Symbol.iterator=$jscomp.global.Symbol("iterator"));"function"!=typeof Array.prototype[a]&&$jscomp.defineProperty(Array.prototype,a,{configurable:!0,writable:!0,value:function(){return $jscomp.iteratorPrototype($jscomp.arrayIteratorImpl(this))}});$jscomp.initSymbolIterator=function(){}};
-$jscomp.initSymbolAsyncIterator=function(){$jscomp.initSymbol();var a=$jscomp.global.Symbol.asyncIterator;a||(a=$jscomp.global.Symbol.asyncIterator=$jscomp.global.Symbol("asyncIterator"));$jscomp.initSymbolAsyncIterator=function(){}};$jscomp.iteratorPrototype=function(a){$jscomp.initSymbolIterator();a={next:a};a[$jscomp.global.Symbol.iterator]=function(){return this};return a};
-$jscomp.iteratorFromArray=function(a,b){$jscomp.initSymbolIterator();a instanceof String&&(a+="");var c=0,e={next:function(){if(c<a.length){var d=c++;return{value:b(d,a[d]),done:!1}}e.next=function(){return{done:!0,value:void 0}};return e.next()}};e[Symbol.iterator]=function(){return e};return e};
-$jscomp.polyfill=function(a,b,c,e){if(b){c=$jscomp.global;a=a.split(".");for(e=0;e<a.length-1;e++){var d=a[e];d in c||(c[d]={});c=c[d]}a=a[a.length-1];e=c[a];b=b(e);b!=e&&null!=b&&$jscomp.defineProperty(c,a,{configurable:!0,writable:!0,value:b})}};$jscomp.polyfill("Array.prototype.keys",function(a){return a?a:function(){return $jscomp.iteratorFromArray(this,function(a){return a})}},"es6","es3");
-$jscomp.checkStringArgs=function(a,b,c){if(null==a)throw new TypeError("The 'this' value for String.prototype."+c+" must not be null or undefined");if(b instanceof RegExp)throw new TypeError("First argument to String.prototype."+c+" must not be a regular expression");return a+""};
-$jscomp.polyfill("String.prototype.startsWith",function(a){return a?a:function(a,c){var b=$jscomp.checkStringArgs(this,a,"startsWith");a+="";var d=b.length,f=a.length;c=Math.max(0,Math.min(c|0,b.length));for(var h=0;h<f&&c<d;)if(b[c++]!=a[h++])return!1;return h>=f}},"es6","es3");$jscomp.findInternal=function(a,b,c){a instanceof String&&(a=String(a));for(var e=a.length,d=0;d<e;d++){var f=a[d];if(b.call(c,f,d,a))return{i:d,v:f}}return{i:-1,v:void 0}};
-$jscomp.polyfill("Array.prototype.find",function(a){return a?a:function(a,c){return $jscomp.findInternal(this,a,c).v}},"es6","es3");
-(function(){var a=Math.round(999999999*Math.random()).toString(),b=1,c=1,e=null,d=null,f=[],h=function(a){for(var b=[],c=0;c<arguments.length;++c)b[c-0]=arguments[c];console.group("OvO TAS Tools Log:");console.log.apply(console,$jscomp.arrayFromIterable(b));console.groupEnd()},k=function(){if(!document.getElementById(a)){var b=document.createElement("div"),c={backgroundColor:"rgba(150,10,1,0.7)",width:"10px",height:"10px",position:"absolute",top:"100px",left:"100px"};Object.keys(c).forEach(function(a){b.style[a]=
-c[a]});b.id=a;document.body.appendChild(b)}},g=globalThis.ovoTasTools={init:function(){window.cr_getC2Runtime&&(d=g.runtime=cr_getC2Runtime());d?(g.startUpdate(),h("OvO TAS Tools Initialised!")):alert("this is code only works on OvO!");k()},get version(){return 4},get timescale(){return b},set timescale(a){a!==b&&(c=b,b=a)},isInLevel:function(){return d.running_layout.name.startsWith("Level")},isPaused:function(){if(g.isInLevel())return d.running_layout.layers.find(function(a){return"Pause"===a.name}).visible},
-loadInputs:function(a){f="number"===typeof a[0][0]?a:a.map(function(a,b){return[1*b/60,a]})},get loadedInputs(){return f},playInputs:function(){g.timescale=1;var a=0,b={tick:function(){k();var c=d.dt;a+=c;for(console.log(a,c,f.length);0<f.length&&a>f[0][0];)a-=1/60,f.shift()[1].forEach(function(a){"Restart"===a?c2_callFunction("Menu > Replay"):"Jump"===a?c2_callFunction("Controls > Buffer",["Jump"]):c2_callFunction("Controls > "+a)});0===f.length&&d.untickMe(b)}};d.tickMe(b)},isInEndCard:function(){if(g.isInLevel()){var a=
-d.running_layout.layers.find(function(a){return"End Game"===a.name}),b=d.running_layout.layers.find(function(a){return"End Card"===a.name});return a.visible||b.visible}},pause:function(){g.timescale=0},undoTimescale:function(){g.timescale=c},step:function(){var a=d.timescale;d.timescale=1;d.tick(!0,null,null);d.timescale=a},startUpdate:function(){e=setInterval(g.update,10)},stopUpdate:function(){clearInterval(e);e=null},update:function(){var a=g.timescale;a==d.timescale||!g.isInLevel()||g.isPaused()||
-g.isInEndCard()||(h("Updating timescale"),d.timescale=a)}};g.init()})();
+  // add the text node to the newly created div
+  b.appendChild(newContent);
+
+  document.body.appendChild(b);
+
+    timeScale.init();
+})();
