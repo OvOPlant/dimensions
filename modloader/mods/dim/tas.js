2bade9701669b0011a43a9951589d1c87c2421d7
OvOEdits <151556534+OvOEdits@users.noreply.github.com>
2024-02-20 14:38:25 -0500

    a

--- modloader/mods/dim/tas.js
+++ modloader/mods/dim/tas.js
@@ -61,7 +61,7 @@
                         aaaa.instances[i].my_timescale = runtime.timescale
                 }
             };
-            }else if (runtime.running_layout.layers.find(x=>x.name === "Pause").visible){
+            }else if (runtime.running_layout.layers.find(x=>x.name === "Pause").visible || runtime.running_layout.layers.find(x=>x.name === "Dialogue").visible){
                 runtime.groups_by_name["gameplay - timescale"].subevents[1].actions[1].parameters[0].object = aaaa
             }
         }
